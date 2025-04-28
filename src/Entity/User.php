c0dc8180fe6270997132754506a717e235b925ae
jegan42 <aznchristophe@gmail.com>
2025-04-28 20:55:27 +0200

    fix:  for postgresql

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ORM\Table(name: '`user`')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
